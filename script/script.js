2195058bfb10ea6b1cf382581be16c9e7185a758
Risenes <risenes@gmail.com>
2021-09-16 15:42:09 +0700

    add multi load

--- script/script.js
+++ script/script.js
@@ -1,24 +1,27 @@
+
+
 function upload(selector, options = {}) {
   const input = document.querySelector(selector)
-  // const preview = document.createElement('div')
+
   const car = document.getElementById("car")
   const resiz = document.getElementById("for__resize")
+  const content = document.getElementById("content")
 
-  // preview.classList.add('for__resize')
+
 
   const open = document.createElement('button')
   open.classList.add('btn', 'btn-primary')
   open.textContent = 'Отркыть изображение'
 
-  // if (options.multi) {
-  //   input.setAttribute('multiple', true)
-  // }
+  if (options.multi) {
+    input.setAttribute('multiple', true)
+  }
 
   if (options.accept && Array.isArray(options.accept)) {
     input.setAttribute('accept', options.accept.join(','))
   }
 
-  // car.insertAdjacentElement('afterbegin', preview)
+
   input.insertAdjacentElement('afterend', open)
 
   const triggerInput = () => input.click()
@@ -29,7 +32,7 @@ function upload(selector, options = {}) {
 
     const files = Array.from(event.target.files)
 
-    // resiz.innerHTML = ''
+    content.innerHTML = ''
     const img = document.getElementById('resize__img')
     if (img) {
       img.parentNode.removeChild(img)
@@ -45,10 +48,15 @@ function upload(selector, options = {}) {
 
       reader.onload = ev => {
         const src = ev.target.result
-        resiz.insertAdjacentHTML('afterbegin',
-          `
+        content.insertAdjacentHTML('afterbegin',
+          `<div class="for__resize" id="for__resize" data-name="${file.name}">
             <img src="${src}" alt="${file.name}" class="resize__img" id="resize__img"/>
-            
+            <i class="bi bi-arrow-clockwise" id="bi"></i>
+            <div class="resizer ne" data-name="${file.name}"></div>
+            <div class="resizer nw" data-name="${file.name}"></div>
+            <div class="resizer sw" data-name="${file.name}"></div>
+            <div class="resizer se" data-name="${file.name}"></div>
+          </div>
         `)
         let rotate = document.getElementById('bi')
         let currentRotate = 0;
@@ -60,8 +68,91 @@ function upload(selector, options = {}) {
           img.style.transition = "transform 1s";
         })
 
+        const el = document.querySelector(".for__resize")
+        let isResizeng = false
+        el.addEventListener('mousedown', mousedown)
 
-        
+        function mousedown(e) {
+          window.addEventListener('mousemove', mousemove)
+          window.addEventListener('mouseup', mouseup)
+
+          let prevX = e.clientX;
+          let prevY = e.clientY;
+
+          function mousemove(e) {
+            if (!isResizeng) {
+              let newX = prevX - e.clientX
+              let newY = prevY - e.clientY
+
+              const rect = el.getBoundingClientRect()
+
+              el.style.left = rect.left - newX + "px";
+              el.style.top = rect.top - newY + "px";
+
+              prevX = e.clientX
+              prevY = e.clientY
+            }
+          }
+
+          function mouseup() {
+            window.removeEventListener('mousemove', mousemove)
+            window.removeEventListener('mouseup', mouseup)
+          }
+        }
+        const resizers = document.querySelectorAll(".resizer")
+        let currentResizer;
+        let res = document.getElementById("for__resize")
+
+        for (let resizer of resizers) {
+          resizer.addEventListener('mousedown', mousedown)
+
+          function mousedown(e) {
+            currentResizer = e.target
+            isResizeng = true
+
+            let prevX = e.clientX
+            let prevY = e.clientY
+
+            window.addEventListener('mousemove', mousemove)
+            window.addEventListener('mouseup', mouseup)
+
+            function mousemove(e) {
+
+              const rect = el.getBoundingClientRect()
+
+              if (currentResizer.classList.contains("se") && currentResizer.dataset.name == res.dataset.name) {
+                el.style.width = rect.width - (prevX - e.clientX) + "px"
+                el.style.height = rect.height - (prevY - e.clientY) + "px"
+              }
+              else if (currentResizer.classList.contains("sw") && currentResizer.dataset.name == res.dataset.name) {
+                el.style.width = rect.width + (prevX - e.clientX) + "px"
+                el.style.height = rect.height - (prevY - e.clientY) + "px"
+                el.style.left = rect.left - (prevX - e.clientX) + "px"
+              }
+              else if (currentResizer.classList.contains("ne") && currentResizer.dataset.name == res.dataset.name) {
+                el.style.width = rect.width - (prevX - e.clientX) + "px"
+                el.style.height = rect.height + (prevY - e.clientY) + "px"
+                el.style.top = rect.top - (prevY - e.clientY) + "px"
+              }
+              else if(currentResizer.classList.contains("nw") && currentResizer.dataset.name == res.dataset.name){
+                el.style.width = rect.width + (prevX - e.clientX) + "px"
+                el.style.height = rect.height + (prevY - e.clientY) + "px"
+                el.style.top = rect.top - (prevY - e.clientY) + "px"
+                el.style.left = rect.left - (prevX - e.clientX) + "px"
+              }
+
+              prevX = e.clientX;
+              prevY = e.clientY;
+            }
+
+            function mouseup() {
+              window.removeEventListener('mousemove', mousemove)
+              window.removeEventListener('mouseup', mouseup)
+              isResizeng = false
+            }
+          }
+        }
+
       }
 
       reader.readAsDataURL(file)
@@ -81,92 +172,8 @@ upload('#file', {
 })
 
 
-const el = document.querySelector(".for__resize")
 
-let isResizeng = false
 
-el.addEventListener('mousedown', mousedown)
-
-function mousedown(e) {
-  window.addEventListener('mousemove', mousemove)
-  window.addEventListener('mouseup', mouseup)
-
-  let prevX = e.clientX;
-  let prevY = e.clientY;
-
-  function mousemove(e) {
-    if (!isResizeng) {
-      let newX = prevX - e.clientX
-      let newY = prevY - e.clientY
-
-      const rect = el.getBoundingClientRect()
-
-      el.style.left = rect.left - newX + "px";
-      el.style.top = rect.top - newY + "px";
-
-      prevX = e.clientX
-      prevY = e.clientY
-    }
-  }
-
-  function mouseup() {
-    window.removeEventListener('mousemove', mousemove)
-    window.removeEventListener('mouseup', mouseup)
-  }
-}
-
-const resizers = document.querySelectorAll(".resizer")
-let currentResizer;
-
-for (let resizer of resizers) {
-  resizer.addEventListener('mousedown', mousedown)
-
-  function mousedown(e) {
-    currentResizer = e.target
-    isResizeng = true
-
-    let prevX = e.clientX
-    let prevY = e.clientY
-
-    window.addEventListener('mousemove', mousemove)
-    window.addEventListener('mouseup', mouseup)
-
-    function mousemove(e) {
-
-      const rect = el.getBoundingClientRect()
-
-      if (currentResizer.classList.contains("se")) {
-        el.style.width = rect.width - (prevX - e.clientX) + "px"
-        el.style.height = rect.height - (prevY - e.clientY) + "px"
-      }
-      else if (currentResizer.classList.contains("sw")) {
-        el.style.width = rect.width + (prevX - e.clientX) + "px"
-        el.style.height = rect.height - (prevY - e.clientY) + "px"
-        el.style.left = rect.left - (prevX - e.clientX) + "px"
-      }
-      else if (currentResizer.classList.contains("ne")) {
-        el.style.width = rect.width - (prevX - e.clientX) + "px"
-        el.style.height = rect.height + (prevY - e.clientY) + "px"
-        el.style.top = rect.top - (prevY - e.clientY) + "px"
-      }
-      else {
-        el.style.width = rect.width + (prevX - e.clientX) + "px"
-        el.style.height = rect.height + (prevY - e.clientY) + "px"
-        el.style.top = rect.top - (prevY - e.clientY) + "px"
-        el.style.left = rect.left - (prevX - e.clientX) + "px"
-      }
-
-      prevX = e.clientX;
-      prevY = e.clientY;
-    }
-
-    function mouseup() {
-      window.removeEventListener('mousemove', mousemove)
-      window.removeEventListener('mouseup', mouseup)
-      isResizeng = false
-    }
-  }
-}
 
 
 
